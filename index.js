e22981594b81fc5dfdf7dab7acb7dcb0517e4d68
febririyanto <107037485+febririyanto@users.noreply.github.com>
2022-06-22 22:31:00 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+const spotify = document.querySelector('.spotify');
+const off = document.querySelector('#off');
+const on = document.querySelector('#on');
+const myAudio = document.querySelector('#myAudio');
+
+off.addEventListener('click', () => soundTrack('off'));
+on.addEventListener('click', () => soundTrack('on'));
+
+const soundTrack = (soundState) => {
+    if(soundState === 'off'){
+        on.style.display = 'block';
+        off.style.display = 'none';
+        spotify.style.color = "#08fdd8";
+        myAudio.play();
+    }
+    else if(soundState === 'on'){
+        on.style.display = 'none';
+        off.style.display = 'block';
+        spotify.style.color = "#f50057";
+        myAudio.pause();
+    }
+}
+
+// Play music functionality
+
+const btnQrcode = document.querySelector('.qrcode');
+const btnTimes = document.querySelector('.times');
+const SideNav = document.querySelector('.aside');
+
+
+btnQrcode.addEventListener('click', () => myFunc('open'));
+btnTimes.addEventListener('click', () => myFunc('close'));
+
+const myFunc = (navCondition) => {
+    if(navCondition === 'open'){
+        SideNav.classList.add('show-nav');
+        btnTimes.style.display = "block";
+        btnQrcode.style.display = "none";
+    }
+    else if(navCondition === 'close'){
+            SideNav.classList.remove('show-nav');
+            btnTimes.style.display = "none";
+            btnQrcode.style.display = "block";
+    }
+}
+
+
+// Part 1 javascript functionality ends here
+$(document).ready(function (){
+    if(!$("#myCanvas").tagcanvas({
+        textColour: "#08fdd8",
+        outlineColour: "transparent",
+        reverse: true,
+        depth: 0.8,
+        maxSpeed: 0.05,
+        weight: true,
+    }, "tags")){
+        // something went wrong hide the canvas container,
+        $("#myCanvasContainer");
+    }
+})
